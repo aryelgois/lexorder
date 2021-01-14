8a6aa27b0e98ee3921c80b5561114daab6001c94
Aryel Mota Góis <aryel.gois@gmail.com>
2021-01-14 18:24:55 -0300

    Add LexOrder decode() and encode()

--- src/LexOrder.ts
+++ src/LexOrder.ts
@@ -4,6 +4,11 @@ export default class LexOrder {
     private converter: SymbolConverter
     private spreadLevel: number
 
+    private zeroSymbol: string
+
+    private validatePattern: RegExp
+    private zeroRightPattern: RegExp
+
     constructor (options: LexOrderOptions) {
         this.converter = options.converter
         this.spreadLevel = options.spreadLevel
@@ -17,5 +22,30 @@ export default class LexOrder {
         if (symbols.length < 2) {
             throw new Error('There must be at least 2 symbols.')
         }
+
+        const zeroSymbol = symbols[0]
+
+        if (zeroSymbol === undefined) {
+            throw new Error('Got undefined when reading symbols[0].')
+        }
+
+        this.zeroSymbol = zeroSymbol
+
+        this.validatePattern = RegExp(`^(${symbols.join('|')})*(${symbols.slice(1).join('|')})$`)
+        this.zeroRightPattern = RegExp(`(${zeroSymbol})+$`)
+    }
+
+    decode (word: string) {
+        if (!this.validatePattern.test(word)) {
+            throw new Error(`Argument "${word}" is invalid.`)
+        }
+
+        return this.converter.toBigInt(word)
+    }
+
+    encode (value: bigint, length: number) {
+        return this.converter.fromBigInt(value)
+            .padStart(length, this.zeroSymbol)
+            .replace(this.zeroRightPattern, '')
     }
 }
